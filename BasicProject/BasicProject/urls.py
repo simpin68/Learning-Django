a4483503555fae1634fd6c2c515e752b260a0aef
sphilpin <sphilpin@adairs.com.au>
2018-11-02 16:43:54 +1100

    .

--- BasicProject/BasicProject/urls.py
+++ BasicProject/BasicProject/urls.py
@@ -1,8 +1,25 @@
-from django.conf.urls import include, url
-import HelloDJangoApp.views
+"""
+Definition of urls for BasicProject2.
+"""
 
-# Django processes URL patterns in the order they appear in the array
+from django.conf.urls import include, url
+import HelloDjangoApp.views
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
 urlpatterns = [
-    url(r'^$', HelloDJangoApp.views.index, name='index'),
-    url(r'^home$', HelloDJangoApp.views.index, name='home'),
+    # Examples:
+    # url(r'^$', BasicProject2.views.home, name='home'),
+    # url(r'^BasicProject2/', include('BasicProject2.BasicProject2.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+
+     url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+
 ]
--- BasicProject/HelloDJangoApp/apps.py
+++ BasicProject/HelloDJangoApp/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class HelloDjangoAppConfig(AppConfig):
-    name = 'HelloDjango'
+    name = 'HelloDjangoApp'
